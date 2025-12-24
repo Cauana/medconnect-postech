14ec0c18615384d2ef16101a1d70e1b723613f6e
Wellington Feijo Da Costa <>
2025-12-24 09:52:49 -0300

    Interface usuarioRepository

--- servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/service/UsuarioService.java
+++ servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/service/UsuarioService.java
@@ -1,7 +1,10 @@
 package com.adjt.medconnect.autenticacao.service;
 
+import com.adjt.medconnect.autenticacao.exception.UsuarioJaExisteException;
+import com.adjt.medconnect.autenticacao.exception.UsuarioNaoEncontradoException;
 import com.adjt.medconnect.autenticacao.model.Usuario;
 import com.adjt.medconnect.autenticacao.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -32,7 +35,39 @@ public class UsuarioService implements UserDetailsService {
     }
 
     public Usuario salvar(Usuario usuario) {
+
+        if(usuarioRepository.existsByUsername(usuario.getUsername())){
+            throw new UsuarioJaExisteException("Usuário já cadastrado");
+        }
         usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional
+    public void atualizarSenhaDoUsuarioLogado(String username, String novaSenha){
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(UsuarioNaoEncontradoException::new);
+
+        if(novaSenha == null || novaSenha.isBlank()){
+            throw new IllegalArgumentException("Senha não pode ser vazia");
+        }
+        usuario.setPassword(passwordEncoder.encode(novaSenha));
+        usuarioRepository.save(usuario);
+    }
+
+    @Transactional
+    public void atualizarSenhaPorAdmin(Long id, String novaSenha){
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(UsuarioNaoEncontradoException::new);
+        usuario.setPassword(passwordEncoder.encode(novaSenha));
+        usuarioRepository.save(usuario);
+    }
+
+
+    public void deletar(Long id){
+        if(!usuarioRepository.existsById(id)){
+            throw new IllegalArgumentException("Usuário não encontrado");
+        }
+        usuarioRepository.deleteById(id);
     }
 }
