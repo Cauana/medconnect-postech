87005e70844c264303996cff16b1d41db96b1e5e
Wellington Feijo Da Costa <>
2025-12-31 11:39:24 -0300

    implementação exception

--- servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/exception/GlobalExceptionHandler.java
+++ servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/exception/GlobalExceptionHandler.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.file.AccessDeniedException;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -71,6 +74,17 @@ public class GlobalExceptionHandler {
                         "message",ex.getMessage(),
                         "path",request.getRequestURI()
                 ));
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", ex.getStatusCode().value());
+        body.put("error", "Erro de Validação");
+        body.put("message", ex.getReason()); // Aqui pegamos sua mensagem personalizada
+
+        return new ResponseEntity<>(body, ex.getStatusCode());
     }
 
     @ExceptionHandler(AuthenticationException.class)
